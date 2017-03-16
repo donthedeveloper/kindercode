f8431658672cc75b554bc2b0cc723b33594c1a85
Don Hansen <donhansen347@gmail.com>
2017-03-16 07:27:55 -0500

    progress on procedures reducer

--- app/reducers/commands.jsx
+++ app/reducers/commands.jsx
@@ -1,15 +1,41 @@
 const initialState = {
-  commands: []
+  commands: [],
+  procedure: [],
+  procedureIdCount = 0;
+}
+
+class Node {
+  constructor(id, layerId, commandId, children=[]) {
+    this.id = id;
+    this.layerId = layerId;
+    this.subProcedures = children;
+  }
 }
 
 // constants
 const ADD_COMMAND = 'ADD_COMMAND';
 
+const INSERT_INTO_PROCEDURE = 'INSERT_INTO_PROCEDURE';
+
 // action creaters
 export const addCommand = (text) => ({
   type: ADD_COMMAND,
   text
+});
+
+export const insertIntoProcedure = (layerId, orderInArr commandId) => ({
+  type: INSERT_INTO_PROCEDURE,
+  layerId,
+  orderInArr,
+  commandId
 })
+
+// [procedure, procedure, procedure]
+// procedure ->
+//   id: 2
+//   commandId: 5
+//   subProcedures: [procedure, procedure, procedure]
+
 
 // reducer
 export default (state=initialState, action) => {
@@ -23,7 +49,15 @@ export default (state=initialState, action) => {
       command.text = action.text;
       newState.commands.push(command);
       break;
+    case INSERT_INTO_PROCEDURE:
+      const node = new Node(state.procedureIdCount, action.layerId, action.orderInArr, action.commandId, []);
+      state.procedureIdCount++;
 
+      state.procedure.forEach((procedure) => {
+        if (procedure.layerId ===)
+      });
+
+      break;
     default:
       return state;
   }
