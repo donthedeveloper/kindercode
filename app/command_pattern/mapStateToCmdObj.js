12d2c7dcbb0b8d1bfcfa77e3e1124e9fe37a694b
Soren Yeadon <spyeadon@gmail.com>
2017-03-20 11:51:11 -0500

    now ready for integration test but actually

--- app/command_pattern/mapStateToCmdObj.js
+++ app/command_pattern/mapStateToCmdObj.js
@@ -9,15 +9,15 @@ import {moveX} from './konvaUtils.js';
 //program equals [command1, command2, command3]
 //command2.children equals [nestedCommand1, nestedCommand2]
 
-export function mapStateToCmdObj(func = new FunctionInstance(), program = store.getState().commands.procedure) {
-  if (!program.length) {
-    return func;
-  }
-  else {
-    let nested = program.children;
-    mapStateToCmdObj(func, program.slice(1));
-  }
-}
+// export function mapStateToCmdObj(func = new FunctionInstance(), program = store.getState().commands.procedure) {
+//   if (!program.length) {
+//     return func;
+//   }
+//   else {
+//     let nested = program.children;
+//     mapStateToCmdObj(func, program.slice(1));
+//   }
+// }
 
 // function storeCommand (command) {
 
