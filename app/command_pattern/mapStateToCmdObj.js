c73164128a6d58e7d4ff0ef785272d9fd2e6388d
Sara <sara.m.trujillo@gmail.com>
2017-03-21 15:02:55 -0500

    Update function names for movement

--- app/command_pattern/mapStateToCmdObj.js
+++ app/command_pattern/mapStateToCmdObj.js
@@ -7,17 +7,17 @@ import {MoveX, MoveY, Speak} from './konvaUtils.js';
 
 export function storeCmd (func, command, parent = null) {
   let commandInstance;
-  if (command.id === 0) commandInstance = new MoveUp(); //MoveUp
-  else if (command.id === 1) commandInstance = new MoveDown(); //Move Down
-  else if (command.id === 2) commandInstance = new MoveLeft(); //MoveLeft
-  else if (command.id === 3) commandInstance = new MoveRight(); //MoveRight
+  if (command.id === 0) commandInstance = new MoveYUp(); //MoveUp
+  else if (command.id === 1) commandInstance = new MoveYDown(); //Move Down
+  else if (command.id === 2) commandInstance = new MoveXLeft(); //MoveLeft
+  else if (command.id === 3) commandInstance = new MoveXRight(); //MoveRight
   else if (command.id === 4) commandInstance = new Speak(); //Speak
   else if (command.id === 5) commandInstance = new If(new Condition(command.condition)); //If
   else if (command.id === 6) commandInstance = new Loop(command.input); //Loop
   else {
-    func.storeCommand(new MoveX(300));
+    func.storeCommand(new MoveXLeft());
     func.storeCommand(new Speak('panda'));
-    func.storeCommand(new MoveY(300));
+    func.storeCommand(new MoveYUp());
   }
   if (parent) parent.then(commandInstance);
   // else func.storeCommand(commandInstance)
