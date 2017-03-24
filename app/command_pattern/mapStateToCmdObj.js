0bca005f6114d67248598de174e235d584765b30
Soren Yeadon <spyeadon@gmail.com>
2017-03-24 16:33:16 -0500

    UI reflects all available commands in game engine, red tile still not working

--- app/command_pattern/mapStateToCmdObj.js
+++ app/command_pattern/mapStateToCmdObj.js
@@ -1,8 +1,8 @@
 import {FunctionInstance} from './command.js';
-import {If, Condition} from './conditionals.js';
+import {If, Condition, IfNot} from './conditionals.js';
 import {Loop} from './loops.js';
 import store from '../store.jsx';
-import {MoveXLeft, MoveXRight, MoveYUp, MoveYDown, Speak, CollectStar} from './konvaUtils.js';
+import {MoveXLeft, MoveXRight, MoveYUp, MoveYDown, Speak, CollectStar, CollectStarRedTile} from './konvaUtils.js';
 
 
 export function storeCmd (func, command, parent = null) {
@@ -12,9 +12,11 @@ export function storeCmd (func, command, parent = null) {
   else if (command.commandId === 2) commandInstance = new MoveXLeft(); //MoveLeft
   else if (command.commandId === 3) commandInstance = new MoveXRight(); //MoveRight
   else if (command.commandId === 4) commandInstance = new Speak('pig'); //Speak
-  else if (command.commandId === 5) commandInstance = new If(new Condition(command.condition)); //If
-  else if (command.commandId === 6) commandInstance = new Loop(); //Loop
-  else if (command.commandId === 7) commandInstance = new CollectStar(); //CollectStar
+  else if (command.commandId === 5) commandInstance = new CollectStar(); //Collect Star
+  else if (command.commandId === 6) commandInstance = new CollectStarRedTile(); //CollectOnRedStarTile
+  else if (command.commandId === 7) commandInstance = new If(new Condition('redTile')); //If
+  else if (command.commandId === 8) commandInstance = new IfNot(new Condition('redTile')); //If
+  else if (command.commandId === 9) commandInstance = new Loop(); //Loop
 
   if (parent) parent.then(commandInstance)
   else func.storeCommand(commandInstance)
